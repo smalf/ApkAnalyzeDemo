3de5639fe357d3a8bdb79c8fc85c8b27d8839c77
smalofeev <serhiy.malofeev@weather.com>
2020-09-04 12:34:48 +0300

    Inital project commint

---
+++ app/src/main/java/com/softserveinc/smal/apkanalyzedemo/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.softserveinc.smal.apkanalyzedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    companion object {
+        const val testApiKey = "testtesttesttest1"
+    }
+}
